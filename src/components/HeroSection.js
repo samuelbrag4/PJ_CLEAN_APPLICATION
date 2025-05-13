ca7dfe5d74a430d53f1c72c567c8f349cfdcd8fd
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-05-13 14:21:17 -0300

    feat: create component HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,26 +1,93 @@
-import { View, Image, TouchableOpacity } from "react-native";
+import { View, Image, TouchableOpacity, StyleSheet, Text } from "react-native";
 
 const HeroSection = ({ linkImage, title, text }) => {
     return (
         <View style={styles.container}>
+            {/* Imagem de fundo */}
             <View style={styles.containerFundo}>
-                <Image source={{linkImage}} />
+                <Image source={{ uri: linkImage }} style={styles.image} />
             </View>
+
+            {/* Texto e botões */}
             <View style={styles.containerTexto}>
                 <Text style={styles.title}>{title}</Text>
-                <Text styles={styles.text}>{text}</Text>
+                <Text style={styles.text}>{text}</Text>
 
                 <View style={styles.buttons}>
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={styles.buttonText}>Ler mais</Text>
+                    <TouchableOpacity style={styles.buttonFilled}>
+                        <Text style={styles.buttonTextFilled}>Ler mais</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={styles.buttonText}>Sobre</Text>
+                    <TouchableOpacity style={styles.buttonOutlined}>
+                        <Text style={styles.buttonTextOutlined}>Sobre</Text>
                     </TouchableOpacity>
                 </View>
             </View>
         </View>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FAFAFA",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    containerFundo: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: "center",
+        alignItems: "center",
+        opacity: 0.4, // Opacidade da imagem de fundo
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain",
+    },
+    containerTexto: {
+        padding: 20,
+        zIndex: 1, // Para garantir que o texto fique acima da imagem
+    },
+    title: {
+        fontSize: 23,
+        fontWeight: "bold",
+        color: "#333",
+        marginBottom: 10,
+    },
+    text: {
+        fontSize: 14,
+        color: "#555",
+        marginBottom: 20,
+    },
+    buttons: {
+        flexDirection: "row",
+        gap: 10
+    },
+    buttonFilled: {
+        backgroundColor: "#00DAC7",
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+    },
+    buttonTextFilled: {
+        color: "#fff",
+        fontWeight: "bold",
+    },
+    buttonOutlined: {
+        borderWidth: 1,
+        borderColor: "#00DAC7",
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+    },
+    buttonTextOutlined: {
+        color: "#00DAC7",
+        fontWeight: "bold",
+    },
+});
 
 export default HeroSection;
--- src/screens/BlogScreen.js
+++ src/screens/BlogScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
 
 const BlogScreen = () => {
   return (
@@ -9,9 +10,13 @@ const BlogScreen = () => {
       <Header themeColor="#00DAC7" activePage="Blog" />
 
       {/* Conteúdo principal */}
-      <View style={styles.content}>
-        <Text style={styles.text}>Página Blog</Text>
-      </View>
+      
+      <HeroSection
+        linkImage="https://i.pinimg.com/736x/cc/d7/f1/ccd7f15523a1891ec4b20fc2002221c4.jpg"
+        title="Conheça e encotre dicas incríveis!!"
+        text="Aqui, você encontrará dicas, tendências e informações sobre skincare, produtos de beleza e cuidados com o corpo. Nosso objetivo é ajudar você a escolher os melhores produtos para a sua rotina, entender os ingredientes e descobrir novas formas de cuidar da sua pele. Fique por dentro das novidades e aproveite nossas recomendações para realçar sua beleza da melhor forma!"
+      />
+
     </View>
   );
 };
